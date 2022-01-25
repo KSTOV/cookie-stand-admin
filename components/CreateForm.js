f2f8430f9c282c65ca82e112cf2e3c467afe03d5
KLan Stovall <kland13@gmail.com>
2022-01-24 23:55:06 -0800

    lab complete

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -1,40 +1,51 @@
-export default function CreateForm(props) {
+import useResource from '../hooks/useResource';
+import { useAuth } from '../contexts/auth';
+
+
+export default function CreateForm() {
+    const { user } = useAuth();
+    const { createResource } = useResource();
+
     function handleSubmit(event) {
       event.preventDefault();
-
-      props.table(event.target.location.value, 
-                  event.target.min_cust.value,
-                  event.target.max_cust.value,
-                  event.target.avg_cookie.value)
+      const store = {
+          location: event.target.location.value,
+          min_cust: parseInt(event.target.min_cust.value),
+          max_cust: parseInt(event.target.max_cust.value),
+          avg_cookie: parseFloat(event.target.avg_cookie.value),
+          owner: user.id
+      };
+      createResource(store)
     }
 
     return ( 
-      <form className="text-center mx-80 my-10 px-5 pb-5 bg-green-400 rounded-lg" onSubmit={handleSubmit}>
-        <h1 className="pt-5 pb-2 text-3xl font-medium">Create Cookie Stand</h1>
-  
-        <div className="flex">
-            <label className="p-5 pr-2 font-medium ">Location</label>
-            <input id="location" type="text" className="w-full my-auto mr-10 h-1/2"/>
+      <form className="text-center mx-80 my-5 px-5 pb-5 bg-green-200 rounded-lg border-2 border-green-400" onSubmit={handleSubmit}>
+        
+        <div className='flex justify-evenly p-3'>
+            <div>
+                <label className="p-5 pr-2 font-medium ">Add Location</label>
+                <input placeholder="Cookie Stand Location" name="location" type="text" className="w-full my-auto mr-20 h-1/2"/>
+            </div>
+            
+            <div className="inline-block">
+                <button className="px-20 py-6 ml-10 text-lg font-medium bg-green-600 rounded">Create Stand</button>
+            </div>
         </div>
   
-        <div className="flex m-3">
-            <div className="text-center inline-block w-1/4 mr-5 p-3 bg-green-200 rounded">
+        <div className="flex justify-evenly">
+            <div className="text-center inline-block w-1/4 mr-5 p-3">
                 <p className="block font-medium">Minimum Customers Per Hour</p>
-                <input id="min_cust" type="text" className="w-full"/>
+                <input placeholder="0" name="min_cust" type="text" className="w-full"/>
             </div>
 
-            <div className="text-center inline-block w-1/4 mr-5 p-3 bg-green-200 rounded">
+            <div className="text-center inline-block w-1/4 mr-5 p-3">
                 <p className="block font-medium">Maximum Customers Per Hour</p>
-                <input id="max_cust" type="text" className="w-full"/>
+                <input placeholder="0" name="max_cust" type="text" className="w-full"/>
             </div>
 
-            <div className="text-center inline-block w-1/4 mr-5 p-3 bg-green-200 rounded">
+            <div className="text-center inline-block w-1/4 mr-5 p-3">
                 <p className="block font-medium">Average Cookies Per Sale</p>
-                <input id="avg_cookie" type="text" className="w-full"/>
-            </div>
-
-            <div className="inline-block">
-                <button className="px-20 py-6 text-lg font-medium bg-green-600 rounded">Create</button>
+                <input placeholder="0" name="avg_cookie" type="text" className="w-full"/>
             </div>
         </div>
       </form>
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,37 @@
-import Head from "next/head";
-import { useState } from "react";
-import { hours } from "../data";
-import ReportTable from "../components/ReportTable";
-import CreateForm from "../components/CreateForm";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import { useAuth } from '../contexts/auth'
+import CookieStandAdmin from "../components/CookieStandAdmin"
 
-export default function CookieStandAdmin() {
+export default function Home() {
+  const { user, login, logout } = useAuth()
 
-  const [stores, setStores] = useState([])
+  return (
+    <>
+      {user ? <CookieStandAdmin user={user} logout={logout}/> : <Login login={login}/>}
+    </>
+  )
+}
 
-  function handleTable(location, min_cust, max_cust, avg_cookie) {
-    
-    const store = {
-      location,
-      min_cust,
-      max_cust,
-      avg_cookie,
-      hourly_sales: [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36],
-      id: stores.length
-    }
-
-    setStores([...stores, store])
+function Login({ login }) {
+  async function handleSubmit(event) {
+    event.preventDefault()
+    login(event.target.username.value, event.target.password.value)
   }
 
   return (
-    <div>
-      <Head><title>Cookie Stand Admin</title></Head>
-      <Header title="Cookie Stand Admin"/>
+    <div className="flex items-center justify-center h-screen bg-green-100">
+      <div className="w-1/4 border-2 border-solid p-7 rounded-xl bg-green-300 border-green-500">
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <fieldset>
+            <label className="block m-2 mt-0 font-bold text-center" htmlFor="username">Username</label>
+            <input className="block w-full p-2 m-2 mx-auto" type="text" name="username" placeholder="Username"></input>
+            
+            <label className="block m-2 mt-5 font-bold text-center" htmlFor="password">Password</label>
+            <input className="block w-full p-2 m-2 mx-auto" type="password" name="password" placeholder="Password"></input>
 
-      <main>
-        <CreateForm table={handleTable}/>
-        <ReportTable hours={hours} stores={stores}/>
-      </main>
-
-      <Footer stores={stores}/>
+            <button className="block w-full p-4 mx-auto mt-12 rounded bg-green-500">Login</button>
+          </fieldset>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
